4d5e2b32ca8b91f3d8597e0dfb8751a4b9ba2c3e
justwannaget240fps <elvismusyoka9@gmail.com>
2022-01-28 19:44:56 -0500

    vote for me

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+     apiKey: "AIzaSyC1pKMCya4lzUAmOt5gh94InqVdvW2KNLI",
+     authDomain: "team-voting-96844.firebaseapp.com",
+     projectId: "team-voting-96844",
+     storageBucket: "team-voting-96844.appspot.com",
+     messagingSenderId: "819022676753",
+     appId: "1:819022676753:web:e61bb51ab9e331900082b0"  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
